879880d2be3b6f67bd038bc1a3b166f046898e8c
suniljalandhra <jalandhrasunil99@gmail.com>
2021-06-24 13:39:07 +0530

    After Events And Emit

--- Event.sol
+++ Event.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.4;
 
 contract EventExample{
     mapping(address => uint)public tokenBalance;
+    event TokenSent(address _from,address _to,uint _amount);
     constructor()public {
         tokenBalance[msg.sender] = 100;
         
@@ -13,6 +14,9 @@ contract EventExample{
         assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
         tokenBalance[msg.sender] -= _amount;
         tokenBalance[_to] += _amount;
+        
+        emit TokenSent(msg.sender,_to,_amount);
+        
         return true;
     }
 }
